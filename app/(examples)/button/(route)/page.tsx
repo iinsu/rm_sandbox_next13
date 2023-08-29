ed9d86f983e69915f88aae69186b6b96af74b822
Insu Hong <118152974+iinsu@users.noreply.github.com>
2023-08-29 17:34:43 +0900

    Button 추가

--- app/(examples)/button/(route)/page.tsx
+++ app/(examples)/button/(route)/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Video } from "lucide-react";
+
 const ButtonPage = () => {
   return (
     <>
-      <div>Button Page</div>
+      <div className="m-3">
+        <Button className="flex flex-col h-full" variant="exampleIndigo">
+          <div className="mb-2">
+            <Video size={20} />
+          </div>
+
+          <div>Button 1</div>
+        </Button>
+      </div>
     </>
   );
 };
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
-        test: "bg-indigo-500",
+        exampleIndigo: "bg-indigo-500 text-slate-50 hover:bg-indigo-600",
       },
       size: {
         default: "h-10 px-4 py-2",
