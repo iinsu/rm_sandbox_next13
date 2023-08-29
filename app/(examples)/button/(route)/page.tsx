6cef9c3e56b81668eccebf5000e2ca59b4208f11
Insu Hong <118152974+iinsu@users.noreply.github.com>
2023-08-29 19:21:42 +0900

    버튼에 따라 다르게 스타일 설정

--- app/(examples)/button/(route)/page.tsx
+++ app/(examples)/button/(route)/page.tsx
@@ -1,16 +1,30 @@
 import { Button } from "@/components/ui/button";
-import { Video } from "lucide-react";
+import { Video, AlertTriangle, Info } from "lucide-react";
+
+const buttonStyle = "flex flex-col h-full";
 
 const ButtonPage = () => {
   return (
     <>
-      <div className="flex justify-center m-3">
-        <Button className="flex flex-col h-full" variant="exampleIndigo">
+      <div className="flex justify-center gap-2 m-3">
+        {/* 남색 버튼 */}
+        <Button variant="exampleIndigo" className="flex flex-col h-full">
           <div className="mb-2">
             <Video size={20} />
           </div>
-
           <div>Button 1</div>
+        </Button>
+
+        {/* 빨간 버튼 */}
+        <Button variant="destructive" className="insu-button">
+          <AlertTriangle size={20} className="mb-2" />
+          Button 2
+        </Button>
+
+        {/* 테두리만 있는 버튼 */}
+        <Button variant="outline" className={buttonStyle}>
+          <Info size={20} className="mb-2" />
+          Button 3
         </Button>
       </div>
     </>
